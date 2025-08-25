697a4aa4f477e4fcf1a8a70f763f5ebffdb239c6
Diego-Lui <soto.quispe.057@gmail.com>
2025-08-25 14:27:06 -0500

    s.3. bucles.py

---
+++ S.3 bucles.py
@@ -0,0 +1,16 @@
+import random as rd
+lista= [] # lista vacia
+for i in rango (5): #inicio de un bucle es con el :el identado es importante
+    num=rd.randint(1;10)#genera son numeros aleatorios del 1 al 10
+    lista.append(num)#append añade a ala lista
+print(lista)
+
+vol_sqrt=[]
+V = [4.5,2.32,4.88]
+for i in V:
+    vol_sqrt.append(i*i)
+print(f"el voltaje al cuadrado es: {vol_sqrt}") #comando map
+
+lectura = [4.95, 5.10, 4.88]
+for idx, vol in enumerate(lectura, start=1):
+    print(f"{idx}: {vol:.2f}v")
